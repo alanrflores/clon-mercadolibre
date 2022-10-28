2a8f8a0a30b209dc2a77bc587940a63bf6bc627a
alanrflores <alan.flores640@gmail.com>
2022-10-28 12:27:49 -0300

    update commits

--- src/components/category/CategoryDetail.jsx
+++ src/components/category/CategoryDetail.jsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { fetchingCategoryId } from '../../store/features/categorys/categorySlice';
-import Carrousel from '../carrousel/Carrousel';
-import styles from './category.module.css';
+import { fetchingCategoryId } from "../../store/features/categorys/categorySlice";
+import  "./category.css";
+//import Swiper JS
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
 
 const CategoryDetail = () => {
   const { id } = useParams();
-  const dataId = useSelector((state)=> state.categorys.categoryId);
+  const dataId = useSelector((state) => state.categorys.categoryId);
   const dispatch = useDispatch();
 
-  useEffect(()=> {
-   dispatch(fetchingCategoryId(id));
-  },[]);
+  useEffect(() => {
+    dispatch(fetchingCategoryId(id));
+  }, []);
 
-   console.log(id)
+  console.log(id);
   return (
-    <div className={styles.containerCategory}>
-     <Carrousel />
+    <div className="container-category">
+      <div className="container-swipe">
+        {/* <Swiper navigation={true} modules={[Navigation]} className="my-swiper">
+          <SwiperSlide>
+            <img
+              classname="img-swiper"
+              src="https://http2.mlstatic.com/D_Q_NP_2X_767228-MLA51181835566_082022-G.webp"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              classname="img-swiper"
+              src="src=https://http2.mlstatic.com/D_Q_NP_2X_767228-MLA51181835566_082022-G.webp"
+              alt=""
+            />
+          </SwiperSlide>
+        </Swiper> */}
+      </div>
     </div>
-  )
+  );
 };
 
 export default CategoryDetail;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,7 +2,11 @@ import React, { useEffect } from 'react'
 import { fetchingProduct } from '../../store/features/search/searchSlice'
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import styles from './home.module.css';
+import './home.css'
+//import Swiper JS
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+
 
 
 const Home = () => {
@@ -13,9 +17,54 @@ const Home = () => {
 
 
   return (
-    <div className={styles.containerHome}>
-        <h1>Home</h1>
-    </div>
+    <div className="container-home">
+      <div className="container-swipe">
+     <Swiper navigation={true} modules={[Navigation]} className="my-swiper">
+     
+     <SwiperSlide>
+            <img
+              src="https://http2.mlstatic.com/D_NQ_857005-MLA52160491972_102022-OO.webp"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              classname="img-swiper"
+              src="https://http2.mlstatic.com/D_NQ_821718-MLA52149846786_102022-OO.webp"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              classname="img-swiper"
+              src="https://http2.mlstatic.com/D_NQ_665091-MLA52069208934_102022-OO.webp"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              classname="img-swiper"
+              src="https://http2.mlstatic.com/D_NQ_633479-MLA52070039987_102022-OO.webp"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              classname="img-swiper"
+              src="https://http2.mlstatic.com/D_NQ_771112-MLA52166986829_102022-OO.webp"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              classname="img-swiper"
+              src="https://http2.mlstatic.com/D_NQ_666729-MLA52064400336_102022-OO.webp"
+              alt=""
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+</div>
   )
 }
 
